268b056f231d22534054f9d643e8bdc938cf8b7a
Eric Andrews <ericandrews@FattAdmin2s-MacBook-Air.local>
2016-07-08 17:51:06 -0400

    add body parser and add post route

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var mysql = require('mysql');
-var http = require('http');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
 
 var connection = mysql.createConnection({
   host     : '127.0.0.1',
@@ -9,28 +14,41 @@ var connection = mysql.createConnection({
   port     : 33060
 });
 
+var renderMainPage = function(args) { 
 
-function renderHtml(args) {
   return `
   <!DOCTYPE html>
   <html>
   <head>
-  <title>${args.title}</title>
+    <title>${args.title}</title>
   </head>
   <body>
-  <h1>${args.title}</h1>
-  <table>
-    <tr><th>Name</th><th>D.O.B.</th>
-    ${args.people.join('')}
-  </table>
+
+    <h1>${args.title}</h1>
+    <table>
+      <tr><th>Name</th><th>D.O.B.</th>
+      ${args.people.join('')}
+    </table>
+    <form action="/person" method="post">
+      <input name="name" /><br/>
+      <input name="dob" /><br/>
+      <input value="submit" type="submit" />
+    </form>
   </body>
-  </html>`;
-}
+  </html>
+  `;
+};
 
+app.post('/person', function (req, res) {
+  console.log(req.body);
+  // here intercept the post
+  // req.body.name
+  // req.body.dob
+  // here insert into the people database
+  // and refresh the listing
+});
 
-http.createServer(function (req, res) {
-
-  res.writeHeader(200, {"Content-Type": "text/html"});  
+app.get('/people', function (req, res) {
   var peopleList = [];
 
   connection.query('SELECT * from test2', function(err, people, fields) {
@@ -38,11 +56,11 @@ http.createServer(function (req, res) {
       peopleList.push(`<tr><td>${person.name}</td><td>${person.dob}</td></tr>`);
     })
 
-    res.write(renderHtml({
+    res.send(renderMainPage({
       title: 'Persons of Extra-Interest',
       people: peopleList
     }));
-    res.end();
   });
+})
 
-}).listen(8080);
+app.listen(8080);
