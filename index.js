408f659c70f0b0bcddb081d006c1da207e51713a
Eric Andrews <ericandrews@FattAdmin2s-MacBook-Air.local>
2016-07-08 11:08:45 -0400

    Here we go

---
+++ index.js
@@ -0,0 +1,35 @@
+
+var mysql      = require('mysql');
+
+
+var connection = mysql.createConnection({
+  host     : '127.0.0.1',
+  user     : 'homestead',
+  password : 'secret',
+  database : 'test',
+	port     : 33060
+});
+
+connection.connect();
+
+// connection.query('INSERT INTO table1 VALUES (1, `doggy dog world`)'){
+
+// 	  if (!err)
+// 	    console.log('added!');
+// 	  else
+// 	    console.log('');
+// }
+
+connection.query('SELECT * from table1', function(err, people, fields) {
+
+		people.map(function(person) {
+			console.log(person.id);
+			console.log(person.name);
+		})
+});
+
+
+
+
+
+connection.end();
