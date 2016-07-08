4ae09fefed399ea8ca38dca1b02c24d1fc7ffddd
Eric Andrews <ericandrews@FattAdmin2s-MacBook-Air.local>
2016-07-08 17:33:05 -0400

    adding nodemon and fixing async issues

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 var mysql = require('mysql');
 var http = require('http');
 
@@ -10,19 +9,40 @@ var connection = mysql.createConnection({
   port     : 33060
 });
 
+
+function renderHtml(args) {
+  return `
+  <!DOCTYPE html>
+  <html>
+  <head>
+  <title>${args.title}</title>
+  </head>
+  <body>
+  <h1>${args.title}</h1>
+  <table>
+    <tr><th>Name</th><th>D.O.B.</th>
+    ${args.people.join('')}
+  </table>
+  </body>
+  </html>`;
+}
+
+
 http.createServer(function (req, res) {
 
   res.writeHeader(200, {"Content-Type": "text/html"});  
+  var peopleList = [];
 
   connection.query('SELECT * from test2', function(err, people, fields) {
-      var i = 0;
-      people.map(function(person) {
-        res.write('<br/>' + person.name + ' ' + person.dob);
+    people.map(function(person) {
+      peopleList.push(`<tr><td>${person.name}</td><td>${person.dob}</td></tr>`);
+    })
 
-        if (++i === people.length) {
-          res.end();
-        }
-      })
+    res.write(renderHtml({
+      title: 'Persons of Extra-Interest',
+      people: peopleList
+    }));
+    res.end();
   });
 
 }).listen(8080);
