1ed629f799f0337a3b972e1adf71caf433ad11f4
Daniel Walker <daniel@fattmerchant.com>
2016-07-08 11:57:53 -0400

    get index running with start command

--- index.js
+++ index.js
@@ -1,35 +1,30 @@
 
-var mysql      = require('mysql');
-
+var mysql = require('mysql');
 
 var connection = mysql.createConnection({
   host     : '127.0.0.1',
   user     : 'homestead',
   password : 'secret',
   database : 'test',
-	port     : 33060
+  port     : 33060
 });
 
 connection.connect();
 
 // connection.query('INSERT INTO table1 VALUES (1, `doggy dog world`)'){
 
-// 	  if (!err)
-// 	    console.log('added!');
-// 	  else
-// 	    console.log('');
+//     if (!err)
+//       console.log('added!');
+//     else
+//       console.log('');
 // }
 
-connection.query('SELECT * from table1', function(err, people, fields) {
+connection.query('SELECT * from test2', function(err, people, fields) {
 
-		people.map(function(person) {
-			console.log(person.id);
-			console.log(person.name);
-		})
+    people.map(function(person) {
+      console.log(person.name);
+      console.log(person.dob);
+    })
 });
-
-
-
-
 
 connection.end();
